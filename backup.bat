8a7f71579e91ae1151805d603b648b41fc4c98e9
randomUser134923 <59132862+randomUser134923@users.noreply.github.com>
2020-01-11 23:11:08 +0900

    Add files via upload

--- backup.bat
+++ backup.bat
@@ -1,9 +1,13 @@
-SET Today=%date%
-SET Backup_dir=C:\Users\Cyber\Desktop\backup-%Today%
+@echo off
 
-mkdir C:\Users\Cyber\Desktop\backup-%Today%
-xcopy C:\Users\Cyber\Desktop\testText.txt %Backup_dir%\astest.txt
+SET Backup_dir=C:\Users\Cyber\Desktop\
+SET Backup_list=C:\Users\Cyber\Desktop\testfile.txt
+SET dt=%date%_%time:~0,2%_%time:~3,2%
 
-::forfiles /p "%Backup_dir%" /s /m *.* /D -0 /C "cmd /c del @path"
+IF NOT EXIST %Backup_dir%\backup-%dt% ( mkdir %Backup_dir%\backup-%dt%)
+
+FOR /f "tokens=*" %%a in (%Backup_list%) do ( xcopy %%a %Backup_dir%\backup-%dt% )
+
+FORFILES /P %Backup_dir% /S /D -10 /C "cmd /c IF @isdir == TRUE rd /S /Q @path"
+
 PAUSE
-::cmd /k
